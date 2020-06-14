527fb619708e7b1174bc028b3456a3d5ac8df0de
wnsrb003 <wnsrb003@naver.com>
2020-06-14 16:19:30 +0900

    #roomdetailView

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
 from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.http import Http404
+from django.shortcuts import render
+from django.urls import reverse
 from . import models
 
 
@@ -13,8 +16,9 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition"""
+
+    model = models.Room
